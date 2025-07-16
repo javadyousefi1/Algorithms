645efa3895c26d6e5650245053138bf1e3fa0397
javadyousefi <javadyousefi9000@gmail.com>
2025-07-16 13:42:04 +0330

    first commit

---
+++ Contains Duplicate.js
@@ -0,0 +1,46 @@
+// 217. Contains Duplicate
+
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+// Example 1:
+
+// Input: nums = [1,2,3,1]
+
+// Output: true
+
+// Explanation:
+
+// The element 1 occurs at the indices 0 and 3.
+
+// Example 2:
+
+// Input: nums = [1,2,3,4]
+
+// Output: false
+
+// Explanation:
+
+// All elements are distinct.
+
+// Example 3:
+
+// Input: nums = [1,1,1,3,3,4,3,2,4,2]
+
+// Output: true
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function (nums) {
+    const repeat = new Map();
+
+    for (const item of nums) {
+        if (repeat.get(item)) {
+            return true;
+        }
+        repeat.set(item, true);
+    }
+
+    return false;
+};
